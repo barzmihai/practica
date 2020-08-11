1196a6becc9c4065bd5011bafbb4b119f7cf02c2
barzmihai <mihai.barz@yahoo.com>
2020-08-11 15:48:34 +0200

    db

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+include 'dbconnection.php';
+
+$conn = OpenCon();
+
+echo "Connected Successfully";
+
+CloseCon($conn);
+
+?>
--- movies.php
+++ movies.php
@@ -1,5 +1,3 @@
-
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
