49bf6bafe68b58a10e020d4991b0d17a4d519b8d
barzmihai <mihai.barz@yahoo.com>
2020-08-11 17:14:00 +0200

    pagination

--- movies.php
+++ movies.php
@@ -5,123 +5,169 @@
     <style>
         body {font-family: "FreeSans Regular";
             font-size:1.2em;}
+        .pagination {
+            display: inline-block;
+        }
+
+        .pagination a {
+            color: black;
+            float: left;
+            padding: 8px 16px;
+            text-decoration: none;
+        }
+
+        .pagination a.active {
+            background-color: #204051;
+            color: white;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .pagination a:hover:not(.active) {background-color: #ddd;}
     </style>
     <title>Movies</title>
 </head>
 <div style="background-color: #e7dfd5">
-<!-- Navbar -->
-<div class="w3-container" style="background-color: #204051; color:white;">
-    <div class="w3-bar w3-card">
-        <a href="index.php" class="w3-bar-item">
-            <img src="download.png" alt="MovieLogo" class="w3-image">
-        </a>
-        <a href="index.php" class="w3-bar-item w3-button w3-padding w3-round-large" style="margin:8px;">Home</a>
-        <a href="" class="w3-bar-item w3-button w3-padding w3-hide-small w3-round-large" style="margin:8px;">Movies</a>
-        <a href="#tvshows" class="w3-bar-item w3-button w3-padding w3-hide-small w3-round-large" style="margin:8px;">TV Shows</a>
-        <a href="#celebrities" class="w3-bar-item w3-button w3-padding w3-hide-small w3-round-large" style="margin:8px;">Celebrities</a>
-        <div class="w3-bar-item w3-padding">
-            <input type="text" class="w3-bar-item w3-input w3-round-large" style="background-color: #e7dfd5; width:460px;" placeholder="Search for a movie or series...">
-        </div>
-        <a href="#signin" class="w3-bar-item w3-button w3-padding w3-hide-small w3-right w3-round-large" style="margin:8px;">Sign In</a>
-        <div class="w3-dropdown-hover w3-hide-small w3-right">
-            <button class="w3-padding w3-button w3-round-large" style="margin:8px;" title="More">"Username"<i class="fa fa-caret-down"></i></button>
-            <div class="w3-dropdown-content w3-bar-block w3-card-4">
-                <a href="#" class="w3-bar-item w3-button">Your movies</a>
-                <a href="#" class="w3-bar-item w3-button">Favorites</a>
-                <a href="#" class="w3-bar-item w3-button">Change password</a>
+    <!-- Navbar -->
+    <div class="w3-container" style="background-color: #204051; color:white;">
+        <div class="w3-bar w3-card">
+            <a href="index.php" class="w3-bar-item">
+                <img src="download.png" alt="MovieLogo" class="w3-image">
+            </a>
+            <a href="index.php" class="w3-bar-item w3-button w3-padding w3-round-large" style="margin:8px;">Home</a>
+            <a href="" class="w3-bar-item w3-button w3-padding w3-hide-small w3-round-large" style="margin:8px;">Movies</a>
+            <a href="#tvshows" class="w3-bar-item w3-button w3-padding w3-hide-small w3-round-large" style="margin:8px;">TV Shows</a>
+            <a href="#celebrities" class="w3-bar-item w3-button w3-padding w3-hide-small w3-round-large" style="margin:8px;">Celebrities</a>
+            <div class="w3-bar-item w3-padding">
+                <input type="text" class="w3-bar-item w3-input w3-round-large" style="background-color: #e7dfd5; width:460px;" placeholder="Search for a movie or series...">
+            </div>
+            <a href="#signin" class="w3-bar-item w3-button w3-padding w3-hide-small w3-right w3-round-large" style="margin:8px;">Sign In</a>
+            <div class="w3-dropdown-hover w3-hide-small w3-right">
+                <button class="w3-padding w3-button w3-round-large" style="margin:8px;" title="More">"Username"<i class="fa fa-caret-down"></i></button>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a href="#" class="w3-bar-item w3-button">Your movies</a>
+                    <a href="#" class="w3-bar-item w3-button">Favorites</a>
+                    <a href="#" class="w3-bar-item w3-button">Change password</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="w3-container">
-    <h2>Movies</h2>
-
-    <!-- Sidebar -->
-
-    <div class="w3-sidebar w3-card" style="width:20%; height: 70%; background-color: #c8c2bb">
-        <div class="w3-container">
-            <!--sort by-->
-            <div class="w3-dropdown-hover">
-                <button class="w3-button" style = "background-color: #c8c2bb">Sort by</button>
-                <div class="w3-dropdown-content w3-border">
-                    <a href="#releaseasc" class="w3-bar-item w3-button">Release date ascending</a>
-                    <a href="#releasedsc" class="w3-bar-item w3-button">Release date descending</a>
-                    <a href="#rating" class="w3-bar-item w3-button">Rating</a>
-                    <a href="#nameasc" class="w3-bar-item w3-button">Name ascending</a>
-                    <a href="#namedsc" class="w3-bar-item w3-button">Name descending</a>
-                </div>
+    <div class="w3-container w3-sidebar" style = "background-color: #c8c2bb; width: 20%">
+        <h2>Movies</h2>
+        <!--sort by-->
+        <div class="w3-dropdown-hover">
+            <button class="w3-button" style = "background-color: #c8c2bb">Sort by</button>
+            <div class="w3-dropdown-content w3-border">
+                <a href="#releaseasc" class="w3-bar-item w3-button">Release date ascending</a>
+                <a href="#releasedsc" class="w3-bar-item w3-button">Release date descending</a>
+                <a href="#rating" class="w3-bar-item w3-button">Rating</a>
+                <a href="#nameasc" class="w3-bar-item w3-button">Name ascending</a>
+                <a href="#namedsc" class="w3-bar-item w3-button">Name descending</a>
             </div>
-            <!-- genre-->
-            <div class="w3-dropdown-hover">
-                <button class="w3-button" style = "background-color: #c8c2bb">Genre</button>
-                <div class="w3-dropdown-content w3-border">
-                    <a href="#comedy" class="w3-bar-item w3-button">Comedy</a>
-                    <a href="#action" class="w3-bar-item w3-button">Action</a>
-                    <a href="#drama" class="w3-bar-item w3-button">Drama</a>
-                    <a href="#musical" class="w3-bar-item w3-button">Musical</a>
-                    <a href="#other" class="w3-bar-item w3-button">Other</a>
-                    <a href="#romantic" class="w3-bar-item w3-button">Romantic</a>
-                </div>
+        </div>
+        <br>
+        <!-- genre-->
+        <div class="w3-dropdown-hover">
+            <button class="w3-button" style = "background-color: #c8c2bb">Genre</button>
+            <div class="w3-dropdown-content w3-border">
+                <a href="#comedy" class="w3-bar-item w3-button">Comedy</a>
+                <a href="#action" class="w3-bar-item w3-button">Action</a>
+                <a href="#drama" class="w3-bar-item w3-button">Drama</a>
+                <a href="#musical" class="w3-bar-item w3-button">Musical</a>
+                <a href="#other" class="w3-bar-item w3-button">Other</a>
+                <a href="#romantic" class="w3-bar-item w3-button">Romantic</a>
             </div>
-
-            <div class="w3-padding">
-                    <input type="text" class="w3-bar-item w3-input w3-round-large" style="background-color: #e7dfd5" placeholder="Search for a movie...">
-                </div>
-                <div class="w3-center">
-                    <input type="button" class="w3-btn w3-blue"  value="Search!">
-                </div>
+        </div>
+        <br>
+        <div class="w3-dropdown-hover">
+            <button class="w3-button" style = "background-color: #c8c2bb">Year</button>
+            <div class="w3-dropdown-content w3-border">
+                <a href="#2020" class="w3-bar-item w3-button">2020</a>
+                <a href="#2019" class="w3-bar-item w3-button">2019</a>
+                <a href="#2018" class="w3-bar-item w3-button">2018</a>
+                <a href="#2017" class="w3-bar-item w3-button">2017</a>
+                <a href="#2016" class="w3-bar-item w3-button">2016</a>
+                <a href="#2015" class="w3-bar-item w3-button">2015</a>
+                <a href="#2014" class="w3-bar-item w3-button">2014</a>
+                <a href="#2013" class="w3-bar-item w3-button">2013</a>
+                <a href="#2012" class="w3-bar-item w3-button">2012</a>
+                <a href="#2011" class="w3-bar-item w3-button">2011</a>
+                <a href="#2010" class="w3-bar-item w3-button">2010</a>
+                <a href="#2009" class="w3-bar-item w3-button">2009</a>
+                <a href="#2008" class="w3-bar-item w3-button">2008</a>
+                <a href="#2007" class="w3-bar-item w3-button">2007</a>
+                <a href="#2006" class="w3-bar-item w3-button">2006</a>
+                <a href="#2005" class="w3-bar-item w3-button">2005</a>
+                <a href="#2004" class="w3-bar-item w3-button">2004</a>
+                <a href="#2003" class="w3-bar-item w3-button">2003</a>
+                <a href="#2002" class="w3-bar-item w3-button">2002</a>
+                <a href="#2001" class="w3-bar-item w3-button">2001</a>
+                <a href="#2000" class="w3-bar-item w3-button">2000</a>
+            </div>
         </div>
     </div>
-</div>
-<!-- lista filme -->
-<div class="w3-container" style="margin-left:20%;">
-<div class="w3-row" >
-    <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
-        <img src="poster.jpg" class="w3-round-small" alt="Movie 1" >
-    </div>
-    <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
-        <img src="poster.jpg" class="w3-round-small" alt="Movie 2" >
-    </div>
-    <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
-        <img src="poster.jpg" class="w3-round-small" alt="Movie 3" >
-    </div>
-    <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
-        <img src="poster.jpg" class="w3-round-small" alt="Movie 4" >
-    </div>
+    <!-- lista filme -->
+    <div class="w3-container" style="margin-left:20%;">
+        <div class="w3-row" >
+            <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 1" >
+            </div>
+            <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 2" >
+            </div>
+            <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 3" >
+            </div>
+            <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px; margin:30px;">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 4" >
+            </div>
 
-</div>
+        </div>
 
-  <div class="w3-row" >
-      <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-          <img src="poster.jpg" class="w3-round-small" alt="Movie 1" >
-      </div>
-      <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-          <img src="poster.jpg" class="w3-round-small" alt="Movie 2" >
-      </div>
-      <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-          <img src="poster.jpg" class="w3-round-small" alt="Movie 3" >
-      </div>
-      <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-        <img src="poster.jpg" class="w3-round-small" alt="Movie 4" >
-      </div>
-  </div>
+        <div class="w3-row" >
+            <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 1" >
+            </div>
+            <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 2" >
+            </div>
+            <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 3" >
+            </div>
+            <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 4" >
+            </div>
+        </div>
 
-  <div class="w3-row" >
-      <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-          <img src="poster.jpg" class="w3-round-small" alt="Movie 1" >
-      </div>
-      <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-          <img src="poster.jpg" class="w3-round-small" alt="Movie 2" >
-      </div>
-      <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-          <img src="poster.jpg" class="w3-round-small" alt="Movie 3" >
-      </div>
-      <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
-        <img src="poster.jpg" class="w3-round-small" alt="Movie 4" >
-      </div>
-  </div>
-</div>
+        <div class="w3-row" >
+            <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 1" >
+            </div>
+            <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 2" >
+            </div>
+            <div class="w3-panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 3" >
+            </div>
+            <div class="w3 panel w3-card w3-hover-shadow w3-cell" style="width: 100px; height: 130px">
+                <img src="poster.jpg" class="w3-round-small" alt="Movie 4" >
+            </div>
+        </div>
+        <div class="center">
+            <div class="pagination" >
+                <a href="#">&laquo;</a>
+                <a class="active" href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+
+                <a href="#">9</a>
+                <a href="#">&raquo;</a>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
